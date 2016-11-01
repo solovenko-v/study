d7592485d6be0d0c6fdd99dbc9c02ded5e917e6d
solovenko-v <vadim.solovenko@gmail.com>
2016-11-01 22:28:08 +0200

    change in DSP analog filters

--- Digital Signal Processing/Filters.m
+++ Digital Signal Processing/Filters.m
@@ -2,8 +2,10 @@ function Filters
     
     % Signal generate
     % Frequences
-    omega1 = 1;
-    omega2 = 3;
+%     f1 = 1; % HZ
+%     f2 = 3; % HZ
+    w1 = 1; %2 * pi * f1;
+    w2 = 3; %* pi * f2;
     % Amplitude
     A1 = 3;
     A2 = 5;
@@ -15,37 +17,106 @@ function Filters
     delta = 0.01; % precision
 
     N = 5; % Number of long waves
-    TM = fix(2 * pi * N / min(omega1, omega2)); % longest wave period
+    TM = fix(2 * pi * N / min(w1, w2)); % longest wave period
    
     t = 0 : delta : TM; % time-vector
-    s = A1 * sin(omega1 * t + Phi1) + A2 * sin(omega2 * t + Phi2) + Anoise * rand(1,length(t)); % input signal
+    s = A1 * sin(w1 * t + Phi1) + A2 * sin(w2 * t + Phi2) + Anoise * rand(1,length(t)); % input signal
 
     % FILTERS
     % parameters for lowpass and highpass filters
-    Wn = 0.5; % cutoff frequency
+    Wn = 2; % cutoff frequency
     % parameters for bandpass and bandstop filters
-    W1 = 0.4; % lower band edge 
-    W2 = 0.6; % upper band edge
-    Wo = sqrt(W1 * W2); % center frequency
-    Bw = abs(W2 - W1); % bandwidth
+    Wlo = 0.5; % lower band edge 
+    Wup = 2; % upper band edge
+    Wo = sqrt(Wlo * Wup) % center frequency
+    Bw = abs(Wup - Wlo); % bandwidth
     
     % butter
-    n = 5; % Filter order, specified as an integer scalar.
-    [A,B,C,D] = butter(n,Wn) % lowpass filter with cutoff frequency Wn
-    [Ahp,Bhp,Chp,Dhp] = lp2hp(A,B,C,D,Wn); % highpass filter with cutoff frequency Wn
+    n = 10; % Filter order, specified as an integer scalar.
+    [A,B,C,D] = cheby2(n,30,Wn,'s'); % butter(n,Wn) % lowpass filter with cutoff frequency Wn
+    % cheby2(n,30,Wn,'s'); butter(n,Wn,'s');
+    [Ahp,Bhp,Chp,Dhp] = lp2hp(A,B,C,D,3); % highpass filter with cutoff frequency Wn
     [Abp,Bbp,Cbp,Dbp] = lp2bp(A,B,C,D,Wo,Bw); % bandpass filter with central frequency Wo and bandwidth Bw
     [Abs,Bbs,Cbs,Dbs] = lp2bs(A,B,C,D,Wo,Bw); % bandstop filter with central frequency Wo and bandwidth Bw
     
-    [b,a] = 
-    h = freqs(b,a,w);
-    mag = abs(h);
-    phase = angle(h);
-    phasedeg = phase*180/pi;
-
-    subplot(2,1,1), loglog(w,mag), grid on
-    xlabel 'Frequency (rad/s)', ylabel Magnitude
-    subplot(2,1,2), semilogx(w,phasedeg), grid on
-    xlabel 'Frequency (rad/s)', ylabel 'Phase (degrees)'
+    % LOWPASS FILTER (frequency response)
+    [b,a] = ss2tf(A,B,C,D);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * max(w1,w2)
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,1);
+    plot(w, h, [Wn-0.01 Wn+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))]);
+    title(['Lowpass']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
+    
+    
+    % HIGHPASS FILTER (frequency response)
+    [b,a] = ss2tf(Ahp,Bhp,Chp,Dhp);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * max(w1,w2)
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,2);
+    plot(w, h, [Wn-0.01 Wn+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))]);
+    title(['Highpass']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
+    
+     
+    % BANDPASS FILTER (frequency response)
+    [b,a] = ss2tf(Abp,Bbp,Cbp,Dbp);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * max(w1,w2)
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,3);
+    plot(w, h, [Wlo-0.01 Wlo+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))], [Wup-0.01 Wup+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))]);
+    title(['Bandpass']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
+    
+    
+    % BANDSTOP FILTER (frequency response)
+    [b,a] = ss2tf(Abs,Bbs,Cbs,Dbs);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * max(w1,w2)
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,4);
+    plot(w, h, [Wlo-0.01 Wlo+0.01], [max(mag2db(abs(h))) -10 * max(mag2db(abs(h)))], [Wup-0.01 Wup+0.01], [max(mag2db(abs(h))) -10 * max(mag2db(abs(h)))]);
+    title(['Bandstop']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
     
     
 %     for l = 1:20
