12071252831b498173b259d5d5d67cb857ee5ca1
solovenko-v <vadim.solovenko@gmail.com>
2016-11-15 22:01:43 +0200

    analog filters

--- Digital Signal Processing/Filters.m
+++ Digital Signal Processing/Filters.m
@@ -1,35 +1,23 @@
 function Filters
-    
-    % Signal generate
-    % Frequences
-%     f1 = 1; % HZ
-%     f2 = 3; % HZ
-    w1 = 1; %2 * pi * f1;
-    w2 = 3; %* pi * f2;
-    % Amplitude
-    A1 = 3;
-    A2 = 5;
-    Anoise = 1;
-    % Phases
-    Phi1 = 0;
-    Phi2 = pi / 2;
-    
-    delta = 0.01; % precision
-
-    N = 5; % Number of long waves
-    TM = fix(2 * pi * N / min(w1, w2)); % longest wave period
    
-    t = 0 : delta : TM; % time-vector
-    s = A1 * sin(w1 * t + Phi1) + A2 * sin(w2 * t + Phi2) + Anoise * rand(1,length(t)); % input signal
+    % Frequences
+    Omega = [1 3 8];
+    % Amplitudes
+    Amp = [20 5 2];
+    AmpN = 0; % noise amplitude
+    % Phases
+    Phi = [0 pi/2 pi/3];
 
     % FILTERS
     % parameters for lowpass and highpass filters
-    Wn = 2; % cutoff frequency
+    Wn = 5; % cutoff frequency
     % parameters for bandpass and bandstop filters
-    Wlo = 0.5; % lower band edge 
-    Wup = 2; % upper band edge
+    Wlo = 2; % lower band edge 
+    Wup = 4; % upper band edge
     Wo = sqrt(Wlo * Wup) % center frequency
     Bw = abs(Wup - Wlo); % bandwidth
+    
+    OmegaMax = max(max(Omega, Wn));
     
     % analog lowpass filter prototype
     n = 10; % Filter order, specified as an integer scalar.
@@ -50,7 +38,7 @@ function Filters
     [h,w] = freqs(b,a,4096);
     h = mag2db(abs(h));
     for i = 1:length(w)
-        if w(i) > 1.2 * max(w1,w2)
+        if w(i) > 1.2 * OmegaMax
             w = w(1:i);
             h = h(1:i);
             break
@@ -61,7 +49,7 @@ function Filters
     title(['Lowpass']);
     xlabel('Frequency')
     ylabel('Attenuation (dB)')
-    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
     set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
     
     
@@ -70,7 +58,7 @@ function Filters
     [h,w] = freqs(b,a,4096);
     h = mag2db(abs(h));
     for i = 1:length(w)
-        if w(i) > 1.2 * max(w1,w2)
+        if w(i) > 1.2 * OmegaMax
             w = w(1:i);
             h = h(1:i);
             break
@@ -81,7 +69,7 @@ function Filters
     title(['Highpass']);
     xlabel('Frequency')
     ylabel('Attenuation (dB)')
-    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
     set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
     
      
@@ -90,7 +78,7 @@ function Filters
     [h,w] = freqs(b,a,4096);
     h = mag2db(abs(h));
     for i = 1:length(w)
-        if w(i) > 1.2 * max(w1,w2)
+        if w(i) > 1.2 * OmegaMax
             w = w(1:i);
             h = h(1:i);
             break
@@ -101,7 +89,7 @@ function Filters
     title(['Bandpass']);
     xlabel('Frequency')
     ylabel('Attenuation (dB)')
-    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
     set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
     
     
@@ -110,7 +98,7 @@ function Filters
     [h,w] = freqs(b,a,4096);
     h = mag2db(abs(h));
     for i = 1:length(w)
-        if w(i) > 1.2 * max(w1,w2)
+        if w(i) > 1.2 * OmegaMax
             w = w(1:i);
             h = h(1:i);
             break
@@ -121,71 +109,63 @@ function Filters
     title(['Bandstop']);
     xlabel('Frequency')
     ylabel('Attenuation (dB)')
-    set(gca, 'Xlim', [0 1.2 * max(w1,w2)]);
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
     set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
     
+    % FILTRATION
     
+    % LP
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Alp,Blp,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Clp * x' + Dlp * u';    
+    subplot(2,4,5);
+    plot(t,u,t,y);
     
+    % HP
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Ahp,Bhp,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Chp * x' + Dhp * u';    
+    subplot(2,4,6);
+    plot(t,u,t,y);
+
+    % BP
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, 2*n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Abp,Bbp,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Cbp * x' + Dbp * u';    
+    subplot(2,4,7);
+    plot(t,u,t,y);
+     
+    % BS
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, 2*n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Abs,Bbs,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Cbs * x' + Dbs * u';    
+    subplot(2,4,8);
+    plot(t,u,t,y);   
+     
+end
     
-%     for l = 1:20
-%         T = 10 * l * delta; % период с которым берутся отсчеты сигнала (в целых от дискретизации)
-%         % частота дискретизации
-%         Nt = pi / T;
-%         Tq = 1 / 2 * pi * max(omega1, omega2);
-%         Nn = fix(TM / T);
-%         sK = zeros(1, length(t)); 
-%         for k = 0:Nn
-%             ind = round(k * T / delta + 1);
-%             sK = sK + s(ind) * sinc(t / T - k);
-%         end    
-% 
-%     %     plot(t,s);
-%         plot(t,[s;sK]);
-%         title(['T/Tq = ' num2str(T /Tq)]);
-%         % pause(0.5);
-%         waitforbuttonpress;
-%     end
-%   
-%     s = 
-%     s = 5 * sin 
-%     T = 5; % period
-%     tau = 4; % impulse length (must be less then T)
-%     n = 3; % number of impulse in the signal
-%     c = 100;
-%     t = -T*n:(1/c):T*n; % time vector
-%     A = 3; % signal amplitude
-%     N = 50; % number of adds
-%     s  = zeros(1, 2*T*n*c+1); 
-%     for k = -n:n
-%         s = s + A * (heaviside(t-k*T)-heaviside(t-tau-k*T));
-%     end 
-%     
-%     axes
-%     q = T / tau;
-%     sF = (A / q) * ones(1, 2*T*n*c+1);
-%     for k = 1:N
-%         omega = 2 *  k / T;
-%         sF = sF + 2 * (A / q) * cos(pi * omega * (t - tau / 2)) *sinc(omega * tau / 2);
-%     end
-%     
-%     sx = std(s-sF)
-%     
-%     plot(t,[s;sF]);
-%     s = s + 0.01 * rand([length(s),1]);
-   
-%     S = fft(s);
-%     s1 = ifft(S); 
-%     plot(t,[s;s1]);
-% %     f = Fs*(0:(L/2))/L; 
-%     S = fft(s);
-%     figure
-%     subplot(2,1,1);
-%     plot(t,s)
-% 
-%     subplot(2,1,2);
-%     plot(f,S);
+function dydt = odefcn(t,y,A,B,Omega, Amp, AmpN, Phi)
+    dydt = A * y + B * signal(t, Omega, Amp, AmpN, Phi);
+end   
+ 
+function s = signal(t, Omega, Amp, AmpN, Phi)
+
+    s = 0;
+    for i = 1:length(Omega)
+        s = s + Amp(i) * sin(Omega(i) * t + Phi(i));
+    end
+    s = s + AmpN * rand(1);
     
-%     subplot
-%     S = fft(s, 5);
-%     s1 = ifft(S, 5);
-%     plot(t,[s;s1]);
+end
