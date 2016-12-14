adbbc2382c6421c54dacd80a85ee534c019cc30e
vsolovenko <vasogor@yandex.ru>
2016-12-14 17:45:59 +0200

    2

--- Digital Signal Processing/FiltersDigital.m
+++ Digital Signal Processing/FiltersDigital.m
@@ -1,5 +1,9 @@
 function FiltersDigital
-   
+    
+    % TYPE OF FILTRATION
+    ftype = 0; % difference
+    ftype = 1; % convolution
+
     Np = 3; % number of periods
     % Frequences
     Om = [1 5 20];
@@ -7,8 +11,8 @@ function FiltersDigital
     Amp = [10 3 1];
     AmpN = 0; % noise amplitude    % Phases
     Phi = [0 pi/2 pi/3];
-    fd = 8; % sampling frequency
-    Omd = pi*fd;
+    fs = 8; % sampling frequency
+    Omd = pi*fs;
     
     %
     h_min = -50;
@@ -19,7 +23,7 @@ function FiltersDigital
     Om3 = [Om(3)/Omd 1.01*Om(3)/Omd];
    
     % INPUT SIGNAL
-    T = 1 / fd;
+    T = 1 / fs;
     t = 0:T:Np*max(2*pi./Om);   
     s = signal(t, Om, Amp, AmpN, Phi);
     
@@ -28,13 +32,13 @@ function FiltersDigital
     Whp = 0.3;
     Wbp = [0.1 0.3];
     Wbs = [0.1 0.9];
-    n = 8; % filter order
+    n = 10; % filter order
     Rs = 40; % dB of attenuation
     
     % lowpass
     [b,a] = cheby2(n,Rs,Wlp);
     dataIn = s;
-    dataOut = filter(b,a,dataIn);
+    dataOut = apply_filter(b,a,dataIn,fs,ftype);
     subplot(2,4,1);
     [h,w] = freqz(b,a);
     plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
@@ -50,7 +54,7 @@ function FiltersDigital
     % highpass
     [b,a] = cheby2(n,Rs,Whp,'high');
     dataIn = s;
-    dataOut = filter(b,a,dataIn);
+    dataOut = apply_filter(b,a,dataIn,fs,ftype);
     subplot(2,4,2);
     [h,w] = freqz(b,a);
     plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
@@ -66,7 +70,7 @@ function FiltersDigital
      % bandpass
     [b,a] = cheby2(n,Rs,Wbp,'bandpass');
     dataIn = s;
-    dataOut = filter(b,a,dataIn);
+    dataOut = apply_filter(b,a,dataIn,fs,ftype);
     subplot(2,4,3);
     [h,w] = freqz(b,a);
     plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
@@ -82,7 +86,7 @@ function FiltersDigital
      % bandstop
     [b,a] = cheby2(n,Rs,Wbs,'stop');
     dataIn = s;
-    dataOut = filter(b,a,dataIn);
+    dataOut = apply_filter(b,a,dataIn,fs,ftype);
     subplot(2,4,4);
     [h,w] = freqz(b,a);
     plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
@@ -163,7 +167,6 @@ function FiltersDigital
 
 end
       
-
 function s = signal(t, Omega, Amp, AmpN, Phi)
 
     s = 0;
@@ -171,5 +174,53 @@ function s = signal(t, Omega, Amp, AmpN, Phi)
         s = s + Amp(i) * sin(Omega(i) * t + Phi(i));
     end
     s = s + AmpN * rand(1);
+    
+end
+
+function y = apply_filter(b,a,x,fs,ftype)
+    if ftype == 0
+        y = filter_raznost(b,a,x);
+    else
+        y = filter_convolution(b,a,x,fs);
+    end
+end
+
+function y = filter_raznost(b,a,x)
+    
+    Nt = length(x);
+    y = zeros(1,Nt);
+   
+    P = length(b);
+    Q = length(a);
+    
+    shift = max(P,Q) + 1;
+    x = [zeros(1,shift) x];
+    y = [zeros(1,shift) y];
+    
+    for n = shift:(Nt+shift)
+        for i = 1:P
+            y(n) = y(n) + b(i)*x(n+1-i);
+        end
+        for k = 1:Q
+            y(n) = y(n) - a(k)*y(n-k);
+        end
+    end
+    
+    x = x(shift+1:end);
+    y = y(shift+1:end);
+
+end
+
+function y = filter_convolution(b,a,x,fs)
+    
+    Nt = length(x);
+
+    [h,t] = impz(b,a,Nt,fs);
+    y = zeros(1,Nt);
+    for k = 1:Nt
+        for m = 1:k
+            y(k) = y(k) + x(m) * h(k+1-m);
+        end
+    end
     
 end
