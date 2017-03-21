7fa6c2388ce02c902d9ae891b36c8c6affbd6f61
solovenko-v <vadim.solovenko@gmail.com>
2017-03-21 23:50:57 +0200

    english open lesson

--- Digital Signal Processing/FiltersDigital.m
+++ Digital Signal Processing/FiltersDigital.m
@@ -1,171 +1,173 @@
-function FiltersDigital
-    
-    % TYPE OF FILTRATION
-    ftype = 0; % 0- difference; 1 - convolution
-
-    Np = 3; % number of periods
-    % Frequences
-    Om = [1 5 20];
-    % Amplitudes
-    Amp = [10 3 1];
-    AmpN = 0; % noise amplitude    % Phases
-    Phi = [0 pi/2 pi/3];
-    fs = 8; % sampling frequency
-    Omd = pi*fs;
-    
-    %
-    h_min = -50;
-    h_max = 10;
-    H = [h_min h_max];
-    Om1 = [Om(1)/Omd 1.01*Om(1)/Omd];
-    Om2 = [Om(2)/Omd 1.01*Om(2)/Omd];
-    Om3 = [Om(3)/Omd 1.01*Om(3)/Omd];
+function Filters
    
-    % INPUT SIGNAL
-    T = 1 / fs;
-    t = 0:T:Np*max(2*pi./Om);   
-    s = signal(t, Om, Amp, AmpN, Phi);
-    
-    % IIR FILTERS
-    Wlp = 0.25;
-    Whp = 0.3;
-    Wbp = [0.1 0.3];
-    Wbs = [0.1 0.75];
-    n = 4; % filter order
-    Rs = 40; % dB of attenuation
-    
-    % lowpass
-    [b,a] = cheby2(n,Rs,Wlp);
-    dataIn = s;
-    dataOut = apply_filter(b,a,dataIn,fs,ftype);
-    subplot(2,4,1);
-    [h,w] = freqz(b,a);
-    plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
-    ax = gca;
-    ax.YLim = [h_min h_max];
-    ax.XTick = 0:.5:2;
-    xlabel('Normalized Frequency (\times\pi rad/sample)')
-    ylabel('Magnitude (dB)')
-    title('lowpass');
-    subplot(2,4,5);
-    plot(t, dataIn, t, dataOut);
-    
-    % highpass
-    [b,a] = cheby2(n,Rs,Whp,'high');
-    dataIn = s;
-    dataOut = apply_filter(b,a,dataIn,fs,ftype);
-    subplot(2,4,2);
-    [h,w] = freqz(b,a);
-    plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
-    ax = gca;
-    ax.YLim = [h_min h_max];
-    ax.XTick = 0:.5:2;
-    xlabel('Normalized Frequency (\times\pi rad/sample)')
-    ylabel('Magnitude (dB)')
-    title('highpass');
-    subplot(2,4,6);
-    plot(t, dataIn, t, dataOut); 
-    
-     % bandpass
-    [b,a] = cheby2(n,Rs,Wbp,'bandpass');
-    dataIn = s;
-    dataOut = apply_filter(b,a,dataIn,fs,ftype);
-    subplot(2,4,3);
-    [h,w] = freqz(b,a);
-    plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
-    ax = gca;
-    ax.YLim = [h_min h_max];
-    ax.XTick = 0:.5:2;
-    xlabel('Normalized Frequency (\times\pi rad/sample)')
-    ylabel('Magnitude (dB)')
-    title('bandpass');
-    subplot(2,4,7);
-    plot(t, dataIn, t, dataOut);
-    
-     % bandstop
-    [b,a] = cheby2(n,Rs,Wbs,'stop');
-    dataIn = s;
-    dataOut = apply_filter(b,a,dataIn,fs,ftype);
-    subplot(2,4,4);
-    [h,w] = freqz(b,a);
-    plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
-    ax = gca;
-    ax.YLim = [h_min h_max];
-    ax.XTick = 0:.5:2;
-    xlabel('Normalized Frequency (\times\pi rad/sample)')
-    ylabel('Magnitude (dB)')
-    title('bandstop');
-    subplot(2,4,8);
-    plot(t, dataIn, t, dataOut);
-    
-    figure
-    
-    % FIR FILTERS
-    
-    ord = 44;
-    low = 0.1;
-    bnd = [0.5 0.9];
-    
-    % fir1
-    b = fir1(ord,[low bnd],'DC-1',tukeywin(ord+1));
-    a = 1;
-    dataIn = s;
-    dataOut = apply_filter(b,a,dataIn,fs,ftype); %filter(b,a,dataIn);
-    subplot(2,2,1);
-    [h,w] = freqz(b,a);
-    plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
-    ax = gca;
-    ax.YLim = [h_min h_max];
-    ax.XTick = 0:.5:2;
-    xlabel('Normalized Frequency (\times\pi rad/sample)')
-    ylabel('Magnitude (dB)')
-    title('fir1');
-    subplot(2,2,3);
-    plot(t, dataIn, t, dataOut);
-    
-    f = [0 0.05 0.4 0.6 0.95 1];
-    c = [0 0.0 1.0 1.0 0.0 0];
-    b = firpm(17,f,c);
-    a = 1;
-    dataIn = s;
-    dataOut = apply_filter(b,a,dataIn,fs,ftype); %filter(b,a,dataIn);
-    subplot(2,2,2);
-    [h,w] = freqz(b,a);
-    plot(w/pi,20*log10(abs(h)), Om1, H, Om2, H, Om3, H);
-    ax = gca;
-    ax.YLim = [h_min h_max];
-    ax.XTick = 0:.5:2;
-    xlabel('Normalized Frequency (\times\pi rad/sample)')
-    ylabel('Magnitude (dB)')
-    title('firpm');
-    subplot(2,2,4);
-    plot(t, dataIn, t, dataOut);    
-    
-    % IIR zeros and poles
-    figure
-    
-    [b,a] = cheby2(n,Rs,Wlp);
-    subplot(2,2,1);
-    zplane(b,a);
-    title('lowpass');
-    
-    [b,a] = cheby2(n,Rs,Whp,'high');;
-    subplot(2,2,2);
-    zplane(b,a);
-    title('highpass');
-    
-    [b,a] = cheby2(n,Rs,Wbp,'bandpass');
-    subplot(2,2,3);
-    zplane(b,a);
-    title('bandpass');
-    
-    [b,a] = cheby2(n,Rs,Wbs,'stop');
-    subplot(2,2,4);
-    zplane(b,a);
-    title('bandstop');
+    % Frequences
+    Omega = [1 3 8];
+    % Amplitudes
+    Amp = [20 5 2];
+    AmpN = 0; % noise amplitude
+    % Phases
+    Phi = [0 pi/2 pi/3];
 
+    % FILTERS
+    % parameters for lowpass and highpass filters
+    Wn = 15; % cutoff frequency
+    % parameters for bandpass and bandstop filters
+    Wlo = 2; % lower band edge 
+    Wup = 4; % upper band edge
+    Wo = sqrt(Wlo * Wup) % center frequency
+    Bw = abs(Wup - Wlo); % bandwidth
+    
+    OmegaMax = max(max(Omega, Wn));
+    
+    % analog lowpass filter prototype
+    n = 10; % Filter order, specified as an integer scalar.
+    Rp = 5; % dB of ripple in the passband
+    Rs = 50; % dB of ripple in the stopband
+    [z,p,k] = buttap(n); % returns the zeros, poles, and gain
+    % besselap(n) buttap(n) cheb1ap(n,Rp) cheb2ap(n,Rs) ellipap(n,Rp,Rs)
+    [A,B,C,D] = zp2ss(z,p,k);
+    
+    % transforms analog lowpass filter prototypes
+    [Alp,Blp,Clp,Dlp] = lp2lp(A,B,C,D,Wn); % lowpass filter with cutoff frequency Wn
+    [Ahp,Bhp,Chp,Dhp] = lp2hp(A,B,C,D,Wn); % highpass filter with cutoff frequency Wn
+    [Abp,Bbp,Cbp,Dbp] = lp2bp(A,B,C,D,Wo,Bw); % bandpass filter with central frequency Wo and bandwidth Bw
+    [Abs,Bbs,Cbs,Dbs] = lp2bs(A,B,C,D,Wo,Bw); % bandstop filter with central frequency Wo and bandwidth Bw
+    
+    % LOWPASS FILTER (frequency response)
+    [b,a] = ss2tf(Alp,Blp,Clp,Dlp);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * OmegaMax
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,1);
+    plot(w, h, [Wn-0.01 Wn+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))]);
+    title(['Lowpass']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
+    
+    
+    % HIGHPASS FILTER (frequency response)
+    [b,a] = ss2tf(Ahp,Bhp,Chp,Dhp);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * OmegaMax
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,2);
+    plot(w, h, [Wn-0.01 Wn+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))]);
+    title(['Highpass']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
+    
+     
+    % BANDPASS FILTER (frequency response)
+    [b,a] = ss2tf(Abp,Bbp,Cbp,Dbp);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * OmegaMax
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,3);
+    plot(w, h, [Wlo-0.01 Wlo+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))], [Wup-0.01 Wup+0.01], [max(mag2db(abs(h))) min(mag2db(abs(h)))]);
+    title(['Bandpass']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
+    
+    
+    % BANDSTOP FILTER (frequency response)
+    [b,a] = ss2tf(Abs,Bbs,Cbs,Dbs);
+    [h,w] = freqs(b,a,4096);
+    h = mag2db(abs(h));
+    for i = 1:length(w)
+        if w(i) > 1.2 * OmegaMax
+            w = w(1:i);
+            h = h(1:i);
+            break
+        end
+    end
+    subplot(2,4,4);
+    plot(w, h, [Wlo-0.01 Wlo+0.01], [max(mag2db(abs(h))) -10 * max(mag2db(abs(h)))], [Wup-0.01 Wup+0.01], [max(mag2db(abs(h))) -10 * max(mag2db(abs(h)))]);
+    title(['Bandstop']);
+    xlabel('Frequency')
+    ylabel('Attenuation (dB)')
+    set(gca, 'Xlim', [0 1.2 * OmegaMax]);
+    set(gca, 'Ylim', [1 + min(h) 1 + max(h)]);
+    
+    % FILTRATION
+    
+    % LP
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Alp,Blp,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Clp * x' + Dlp * u';    
+    subplot(2,4,5);
+    plot(t,u,t,y); 
+    xlabel('time')
+    ylabel('Amplitude')
+    
+    % HP
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Ahp,Bhp,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Chp * x' + Dhp * u';    
+    subplot(2,4,6);
+    plot(t,u,t,y); 
+    xlabel('time')
+    ylabel('Amplitude')
+
+    % BP
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, 2*n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Abp,Bbp,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Cbp * x' + Dbp * u';    
+    subplot(2,4,7);
+    plot(t,u,t,y); 
+    xlabel('time')
+    ylabel('Amplitude')
+     
+    % BS
+    opt = odeset('RelTol',1e-4);
+    x0 = zeros(1, 2*n);
+    tspan = [0 20];
+    [t,x] = ode45(@(t,x) odefcn(t,x,Abs,Bbs,Omega, Amp, AmpN, Phi), tspan, x0, opt);
+    u = signal(t, Omega, Amp, AmpN, Phi);
+    y = Cbs * x' + Dbs * u';    
+    subplot(2,4,8);
+    plot(t,u,t,y);
+    xlabel('time')
+    ylabel('Amplitude')  
+     
 end
-      
+    
+function dydt = odefcn(t,y,A,B,Omega, Amp, AmpN, Phi)
+    dydt = A * y + B * signal(t, Omega, Amp, AmpN, Phi);
+end   
+ 
 function s = signal(t, Omega, Amp, AmpN, Phi)
 
     s = 0;
@@ -173,55 +175,5 @@ function s = signal(t, Omega, Amp, AmpN, Phi)
         s = s + Amp(i) * sin(Omega(i) * t + Phi(i));
     end
     s = s + AmpN * rand(1);
-    
-end
-
-function y = apply_filter(b,a,x,fs,ftype)
-    if ftype == 0
-        y = filter_dif(b,a,x);
-    else
-        y = filter_convolution(b,a,x,fs);
-    end
-end
-
-function y = filter_dif(b,a,x)
-    
-    Nt = length(x);
-    y = zeros(1,Nt);
-   
-    P = length(b);
-    Q = length(a);
-    
-    shift = max(P,Q) + 1;
-    x = [zeros(1,shift) x];
-    y = [zeros(1,shift) y];
-    
-    for n = shift:Nt+shift
-        for i = 1:P
-            y(n) = y(n) + b(i)*x(n+1-i);
-        end
-        for k = 2:Q
-            y(n) = y(n) - a(k)*y(n+1-k);
-        end
-        y(n) = 1 / a(1) * y(n);
-        a(1)
-    end
-    
-    x = x(shift+1:end);
-    y = y(shift+1:end);
-
-end
-
-function y = filter_convolution(b,a,x,fs)
-    
-    Nt = length(x);
-
-    [h,t] = impz(b,a,Nt,fs);
-    y = zeros(1,Nt);
-    for k = 1:Nt
-        for m = 1:k
-            y(k) = y(k) + x(m) * h(k+1-m);
-        end
-    end
     
 end
